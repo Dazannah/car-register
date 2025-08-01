fb5a1c05fc1dd7b79dae7c1520d867a43037ecec
Dazannah <beattius@gmail.com>
2025-08-01 20:15:48 +0200

    changed to hungary

--- resources/views/livewire/settings/appearance.blade.php
+++ resources/views/livewire/settings/appearance.blade.php
@@ -9,11 +9,12 @@ new class extends Component {
 <section class="w-full">
     @include('partials.settings-heading')
 
-    <x-settings.layout :heading="__('Appearance')" :subheading=" __('Update the appearance settings for your account')">
+    <x-settings.layout :heading="__('Megjelenés')" :subheading="__('Frissítsd a fiókod megjelenési beállításait')">
         <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
-            <flux:radio value="light" icon="sun">{{ __('Light') }}</flux:radio>
-            <flux:radio value="dark" icon="moon">{{ __('Dark') }}</flux:radio>
-            <flux:radio value="system" icon="computer-desktop">{{ __('System') }}</flux:radio>
+            <flux:radio class="hover:cursor-pointer" value="light" icon="sun">{{ __('Világos') }}</flux:radio>
+            <flux:radio class="hover:cursor-pointer" value="dark" icon="moon">{{ __('Sötét') }}</flux:radio>
+            <flux:radio class="hover:cursor-pointer" value="system" icon="computer-desktop">{{ __('Rendszer') }}
+            </flux:radio>
         </flux:radio.group>
     </x-settings.layout>
 </section>

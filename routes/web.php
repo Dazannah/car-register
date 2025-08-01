32ea6e06d42d19dc714ddc90b71f9728ef71c78b
Dazannah <beattius@gmail.com>
2025-08-01 20:16:30 +0200

    added history route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('history', 'history')
+    ->middleware(['auth', 'verified'])
+    ->name('history');
+
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 

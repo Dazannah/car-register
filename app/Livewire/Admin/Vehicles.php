ab5ecb60c6640b20d3dc1b8532dc53430b408bc1
Dazannah <beattius@gmail.com>
2025-08-01 21:32:46 +0200

    livewire vehicles added

---
+++ app/Livewire/Admin/Vehicles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class Vehicles extends Component {
+    public function render() {
+        return view('livewire.admin.vehicles');
+    }
+}

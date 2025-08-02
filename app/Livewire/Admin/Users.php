9d248117b11d36918bccf700b48152f82e5b2f04
Dazannah <beattius@gmail.com>
2025-08-02 19:19:16 +0200

    added statuses

--- app/Livewire/Admin/Users.php
+++ app/Livewire/Admin/Users.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Status;
 use App\Models\User;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -9,13 +10,27 @@ use Livewire\WithPagination;
 class Users extends Component {
     use WithPagination;
 
+    public function mount() {
+    }
+
+    protected $listeners = ['remount_users'];
+
+    public function remount_users() {
+        $this->mount();
+    }
+
     public function filter_users() {
-        return User::select(['name', 'username', 'status_id'])->with('status')->paginate(10);
+        return User::select(['id', 'name', 'username', 'status_id'])->with(['status', 'vehicles'])->paginate(10);
+    }
+
+    public function statuses() {
+        return Status::all();
     }
 
     public function render() {
         return view('livewire.admin.users', [
-            'users' => $this->filter_users()
+            'users' => $this->filter_users(),
+            'statuses' => $this->statuses()
         ]);
     }
 }

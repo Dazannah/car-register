37c9d3182f39066e7c589c9ff057c4a5c1329430
Dazannah <beattius@gmail.com>
2025-08-02 15:41:12 +0200

    dashboard automatically set current datetime

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -4,10 +4,15 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
-class Dashboard extends Component
-{
-    public function render()
-    {
+class Dashboard extends Component {
+    public string $pickup_time;
+
+    public function mount() {
+        date_default_timezone_set("Europe/Budapest");
+        $this->pickup_time = date("Y-m-d H:i");
+    }
+
+    public function render() {
         return view('livewire.dashboard');
     }
 }

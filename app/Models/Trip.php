1319d49e612450e9aedc6c37ab0c38f02fb6a7a2
Dazannah <beattius@gmail.com>
2025-08-03 10:58:44 +0200

    added trip

---
+++ app/Models/Trip.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\CustomBoolean;
+use App\Casts\CustomPrimaryKey;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model {
+    const CREATED_AT = 'pickup_at';
+    const UPDATED_AT = 'return_at';
+
+    protected $fillable = [
+        'user_id',
+        'vehicle_id',
+        'pickup_at',
+        'return_at',
+        'is_closed'
+    ];
+
+    protected $casts = [
+        'user_id' => CustomPrimaryKey::class,
+        'vehicle_id' => CustomPrimaryKey::class,
+        'is_closed' => CustomBoolean::class
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function vehicle() {
+        return $this->belongsTo(Vehicle::class);
+    }
+}

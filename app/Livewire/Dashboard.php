3cfba726468fd959de8f722d6b794638a26b8b09
Dazannah <beattius@gmail.com>
2025-08-03 11:45:09 +0200

    user_vehicles query made

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,14 +2,29 @@
 
 namespace App\Livewire;
 
+use App\Models\Vehicle;
 use Livewire\Component;
+use Illuminate\Database\Eloquent\Collection;
 
 class Dashboard extends Component {
     public string $pickup_time;
+    public Collection $user_vehicles;
 
     public function mount() {
         date_default_timezone_set("Europe/Budapest");
         $this->pickup_time = date("Y-m-d H:i");
+        $this->user_vehicles = Vehicle::with([
+            'users',
+            'trips' =>
+            function ($query) {
+                $query->where('is_closed', false);
+            }
+        ])->whereHas(
+            'users',
+            function ($query) {
+                return $query->where('id', auth()->user()->id);
+            }
+        )->get();
     }
 
     public function render() {

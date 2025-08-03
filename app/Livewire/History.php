9edbe46c13f3dc06cac984f9356dc2c76251868f
Dazannah <beattius@gmail.com>
2025-08-03 13:29:09 +0200

    jistory created

--- app/Livewire/History.php
+++ app/Livewire/History.php
@@ -2,12 +2,52 @@
 
 namespace App\Livewire;
 
+use App\Models\Trip;
 use Livewire\Component;
+use Livewire\Attributes\Url;
+use Livewire\WithPagination;
 
-class History extends Component
-{
-    public function render()
-    {
-        return view('livewire.history');
+class History extends Component {
+    use WithPagination;
+
+    #[URL(as: 'pickup_at')]
+    public string $filter_pickup_at;
+    #[URL(as: 'return_at')]
+    public string $filter_return_at;
+    #[URL(as: 'licence_plate')]
+    public string $filter_licence_plate;
+
+    protected $listeners = ['reset_history_filter'];
+
+    public function reset_history_filter() {
+        $this->reset('filter_name', 'filter_pickup_at', 'filter_return_at', 'filter_licence_plate');
+        $this->resetPage();
+    }
+
+    public function filter_trips() {
+        return Trip::with(['vehicle'])->where('user_id', auth()->user()->id)->orderBy('return_at', 'desc')->when(
+            isset($this->filter_pickup_at) && !empty($this->filter_pickup_at),
+            function ($query) {
+                return $query->where('pickup_at', 'LIKE', "%$this->filter_pickup_at%");
+            }
+        )->when(
+            isset($this->filter_return_at) && !empty($this->filter_return_at),
+            function ($query) {
+                return $query->where([['is_closed', '=', true], ['return_at', 'LIKE', "%$this->filter_return_at%"]]);
+            }
+        )->when(
+            isset($this->filter_licence_plate) && !empty($this->filter_licence_plate),
+            function ($query) {
+                return $query->whereHas('vehicle', function ($inside_query) {
+                    return $inside_query->where('licence_plate', 'LIKE', "%$this->filter_licence_plate%");
+                });
+            }
+        )->paginate(10);
+    }
+
+    public function render() {
+        return view('livewire.history', [
+            'trips' => $this->filter_trips()
+        ]);
     }
 }
--- resources/views/livewire/history.blade.php
+++ resources/views/livewire/history.blade.php
@@ -16,29 +16,29 @@
                     <tr>
                         <th
                             class="p-2 transition-colors border-y border-slate-200 bg-slate-50 hover:bg-slate-100 dark:bg-neutral-900 dark:hover:bg-neutral-800">
-                            <flux:input size="sm" placeholder="Rendszám" />
+                            <flux:input size="sm" placeholder="Rendszám" wire:model.live="filter_licence_plate" />
                         </th>
                         <th
                             class="p-2 transition-colors border-y border-slate-200 bg-slate-50 hover:bg-slate-100 dark:bg-neutral-900 dark:hover:bg-neutral-800">
                             <flux:field>
                                 <flux:label>Felvéve</flux:label>
-                                <input class="text-slate-700 dark:text-gray-200" type="datetime-local"
-                                    name="pickup-time">
+                                <input class="text-slate-700 dark:text-gray-200" type="date"
+                                    wire:model.live="filter_pickup_at">
                             </flux:field>
                         </th>
                         <th
                             class="p-2 transition-colors border-y border-slate-200 bg-slate-50 hover:bg-slate-100 dark:bg-neutral-900 dark:hover:bg-neutral-800">
                             <flux:field>
                                 <flux:label>Leadva</flux:label>
-                                <input class="text-slate-700 dark:text-gray-200" type="datetime-local"
-                                    name="return-time">
+                                <input class="text-slate-700 dark:text-gray-200" type="date"
+                                    wire:model.live="filter_return_at">
                             </flux:field>
                         </th>
 
 
                         <th
                             class="p-2 transition-colors border-y border-slate-200 bg-slate-50 hover:bg-slate-100 dark:bg-neutral-900 dark:hover:bg-neutral-800">
-                            <flux:button @click.prevent="reset_users_filter" variant="primary"
+                            <flux:button @click.prevent="$wire['reset_history_filter']()" variant="primary"
                                 class="w-full hover:cursor-pointer">
                                 <flux:icon.list-restart />
                             </flux:button>
@@ -46,56 +46,51 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td class="p-2 border-b border-slate-200">
-                            <div class="flex items-center gap-3">
-                                <div class="flex flex-col p-1 bg-white dark:bg-neutral-900">
-                                    <p class="text-sm font-semibold text-slate-700 dark:text-gray-200 uppercase">
-                                        UPK502
-                                    </p>
+                    @foreach ($trips as $trip)
+                        <tr>
+                            <td class="p-2 border-b border-slate-200">
+                                <div class="flex items-center gap-3">
+                                    <div class="flex flex-col p-1 bg-white dark:bg-neutral-900">
+                                        <p class="text-sm font-semibold text-slate-700 dark:text-gray-200 uppercase">
+                                            {{ $trip->vehicle->licence_plate }}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td class="p-2 border-b border-slate-200">
-                            <div class="flex items-center gap-3">
+                            </td>
+                            <td class="p-2 border-b border-slate-200">
+                                <div class="flex items-center gap-3">
+                                    <div class="flex flex-col">
+                                        <input class="text-slate-700 dark:text-gray-200" type="datetime-local"
+                                            value="{{ $trip->pickup_at }}" disabled>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="p-2 border-b border-slate-200">
                                 <div class="flex flex-col">
-                                    <input class="text-slate-700 dark:text-gray-200" type="datetime-local"
-                                        name="return-time" value="2025-08-01 09:05" disabled>
-                                </div>
-                            </div>
-                        </td>
-                        <td class="p-2 border-b border-slate-200">
-                            <div class="flex flex-col">
-                                <input class="text-slate-700 dark:text-gray-200" type="datetime-local"
-                                    name="return-time" value="2025-08-01 14:00" disabled>
-                            </div>
-                        </td>
+                                    @if ($trip->is_closed)
+                                        <input class="text-slate-700 dark:text-gray-200" type="datetime-local"
+                                            value="{{ $trip->return_at }}" disabled>
+                                    @endif
 
-                        <td class="p-2 border-b border-slate-200">
-                            {{-- <button @click.prevent="show_update = true; update_history_id = {{ 1 }}"
+                                </div>
+                            </td>
+
+                            <td class="p-2 border-b border-slate-200">
+                                {{-- <button @click.prevent="show_update = true; update_history_id = {{ 1 }}"
                                     class="relative cursor-pointer h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-slate-900 dark:text-gray-200 transition-all hover:bg-slate-900/10 active:bg-slate-900/20 dark:hover:bg-slate-400/10 dark:active:bg-slate-400/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                                     type="button">
                                     <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
                                         <flux:icon.pencil />
                                     </span>
                                 </button> --}}
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
         <div class="flex items-center justify-between p-3">
-            <p class="block text-sm text-slate-500 dark:text-gray-400">
-                Összesen: 10
-            </p>
-            <div class="flex gap-1">
-                <flux:button @click.prevent="prewious_page" class="w-full hover:cursor-pointer">
-                    <flux:icon.chevron-left />
-                </flux:button>
-                <flux:button @click.prevent="next_page" class="w-full hover:cursor-pointer">
-                    <flux:icon.chevron-right />
-                </flux:button>
-            </div>
+            {{ $trips->links() }}
         </div>
     </div>
 

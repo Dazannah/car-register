97e2320ab07268cd0b0ea973f2e5bb63f266e497
Dazannah <beattius@gmail.com>
2025-08-10 16:55:12 +0200

    Schedule added to close trips daly at 00:10 if returned_at done

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use Carbon\Carbon;
+use App\Models\Trip;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::call(function () {
+    $now = Carbon::now('UTC');
+
+    Trip::withoutTimestamps(function () use ($now) {
+        Trip::where([['is_closed', false], ['return_at', '<=', $now]])->update(['is_closed' => true]);
+    });
+})->dailyAt('00:10');
